acaffd37fe97c61837de5d38f98dd9faec9b8fcc
Gspr01 <110982057+Gspr01@users.noreply.github.com>
2024-09-17 20:41:39 -0300

    Add files via upload

---
+++ Material_Practico/TP6/ImplementacionUS/ImplementacionUS/EmailService.cs
@@ -0,0 +1,42 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using TuProyecto.Models;
+
+public class EmailService
+{
+    private readonly IConfiguration _configuration;
+
+    public EmailService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task SendEmailAsync(string recipientEmail, string subject, string messageBody)
+    {
+        var email = new MimeMessage();
+        email.From.Add(new MailboxAddress(_configuration["tangoapp.noreply@gmail.com"], _configuration["implementacionUS"]));
+        email.To.Add(MailboxAddress.Parse(recipientEmail));
+        email.Subject = subject;
+
+        var bodyBuilder = new BodyBuilder
+        {
+            HtmlBody = messageBody
+        };
+        email.Body = bodyBuilder.ToMessageBody();
+
+        using var smtp = new SmtpClient();
+        try
+        {
+            await smtp.ConnectAsync(_configuration["SmtpSettings:Server"], int.Parse(_configuration["SmtpSettings:Port"]), MailKit.Security.SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_configuration["SmtpSettings:Username"], _configuration["SmtpSettings:Password"]);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
